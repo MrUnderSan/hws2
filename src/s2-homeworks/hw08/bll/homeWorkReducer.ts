139e88d215b8d5a4e6c304556a5142f15a7c06f1
MrUnderSan <MrUnderSan@gmail.com>
2023-10-26 21:23:58 +0300

    completed 11-03-HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const stateCopy = [...state]
+            switch (action.payload)
+            {
+                case 'up':
+                    return stateCopy.sort((a, b) => a.name.localeCompare(b.name))
+                case 'down':
+                    return stateCopy.sort((a, b) => b.name.localeCompare(a.name))
+                default:
+                    return state
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
